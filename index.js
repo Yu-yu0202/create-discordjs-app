6660c9e37d2f06b0a635dc66383aabb20de5b2be
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2025-06-28 16:16:13 +0900

    fix: dotenv load

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Client } from "discord.js";
-import(dotenv).then(d => {
-    d.config();
-});
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new Client({
     intents: []
